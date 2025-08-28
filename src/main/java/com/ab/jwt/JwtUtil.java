ac48961e2ce0f2e06bbedf7d12aa5e8c8513b3f4
bhavik bhatia <bbhatia@asite.com>
2025-08-29 01:03:54 +0530

    JWT Util, pushing code

--- src/main/java/com/ab/jwt/JwtUtil.java
+++ src/main/java/com/ab/jwt/JwtUtil.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
